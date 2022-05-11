d98f4d9c58708d20027a04ec228d7bebe63e976a
Christian Nuss <christian.nuss@gmail.com>
2022-05-10 19:10:09 -0700

    simpler remote state

--- main.tf
+++ main.tf
@@ -14,10 +14,6 @@ data "terraform_remote_state" "example" {
 
   config = {
     address        = "https://api.tfstate.dev/github/v1"
-    lock_address   = "https://api.tfstate.dev/github/v1/lock"
-    unlock_address = "https://api.tfstate.dev/github/v1/lock"
-    lock_method    = "PUT"
-    unlock_method  = "DELETE"
     username       = "tfstate/example"
   }
 }
